ea7bfd78dd95e5f23a868c703b2978dfd7ac0c17
sandeepnain00111-code <sandeepnain00111@gmail.com>
2025-12-02 12:33:16 +0530

    Add files via upload
    covid-19

---
+++ covid/asset/css/opee.java
@@ -0,0 +1,11 @@
+public class main{
+    public static void main(String args[]){
+        int a=5;
+        System.out.println("a");
+        for(int a=5; a<10; a++){
+            if(a%2==0){
+                System.out.println(a);
+            }
+        }
+    }
+}
